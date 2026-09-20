4396305d589caf00493bf98a350b46cdd36a7a50
bramderks <bram.derks@outlook.com>
2026-09-20 08:56:13 +0200

    Add ISELTO brand strategy

---
+++ lib/brand.ts
@@ -0,0 +1,47 @@
+export const brand = {
+  name: "MFP ISELTO",
+  shortName: "ISELTO",
+  experience: "25 jaar ervaring in de printbranche",
+  mission:
+    "Wij helpen organisaties slimmer werken met betrouwbare print-, scan- en documentoplossingen die passen bij hun mensen, processen en ambities.",
+  vision:
+    "Print is geen los apparaat, maar onderdeel van de manier waarop een organisatie informatie verwerkt. ISELTO wil de partner zijn die techniek begrijpelijk maakt en documentstromen steeds eenvoudiger, veiliger en efficiënter laat werken.",
+  promise:
+    "Persoonlijk advies, passende techniek en service die verder gaat dan alleen de machine.",
+  values: [
+    {
+      title: "Persoonlijk",
+      text: "We luisteren eerst naar de organisatie en adviseren vanuit de praktijk."
+    },
+    {
+      title: "Deskundig",
+      text: "Ervaring en technische kennis vertalen we naar duidelijke keuzes zonder onnodige complexiteit."
+    },
+    {
+      title: "Betrouwbaar",
+      text: "Afspraken, continuïteit en bereikbare service vormen de basis van onze samenwerking."
+    },
+    {
+      title: "Praktisch",
+      text: "Elke oplossing moet in de dagelijkse praktijk werken en aantoonbaar waarde toevoegen."
+    }
+  ],
+  strategy: [
+    {
+      title: "1. Begrijpen",
+      text: "We brengen volumes, werkprocessen, gebruikers, locaties, beveiliging en toekomstige behoeften in kaart."
+    },
+    {
+      title: "2. Adviseren",
+      text: "We vertalen die informatie naar een overzichtelijke keuze van machines, software en dienstverlening."
+    },
+    {
+      title: "3. Inrichten",
+      text: "We zorgen voor installatie, configuratie, koppelingen en instructie zodat de oplossing goed wordt gebruikt."
+    },
+    {
+      title: "4. Optimaliseren",
+      text: "We blijven kijken naar verbruik, prestaties, documentstromen en veranderingen binnen de organisatie."
+    }
+  ]
+} as const;
