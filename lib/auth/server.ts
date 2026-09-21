e63df90bec955dd3ff666a388be556fea813e0ec
bramderks <bram.derks@outlook.com>
2026-09-21 14:16:37 +0200

    Mijn Iselto basis: lib/auth/server.ts

---
+++ lib/auth/server.ts
@@ -0,0 +1,6 @@
+import {createNeonAuth} from "@neondatabase/auth/next/server";
+
+export const auth=createNeonAuth({
+  baseUrl:process.env.NEON_AUTH_BASE_URL!,
+  cookies:process.env.NEON_AUTH_COOKIE_SECRET?{secret:process.env.NEON_AUTH_COOKIE_SECRET,sessionDataTtl:300}:undefined,
+});
